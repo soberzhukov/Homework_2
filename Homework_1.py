42d836c3b2fc5739e89421b1547b10cd451ffd6e
Sergei Zhukov <soberzhukov@mail.ru>
2020-11-26 00:45:41 +0400

    initial ex_2

--- Homework_1.py
+++ Homework_1.py
@@ -1,5 +1,5 @@
-def create_cook_book(document):
-    with open(document, encoding='utf8') as f:
+def cook_book():
+    with open('recipes.txt', encoding='utf8') as f:
         cook_book = {}
         while True:
             dish = f.readline().strip('\n')
@@ -8,7 +8,7 @@ def create_cook_book(document):
             a = int(f.readline())
             while a != 0:
                 ingredient = f.readline().split('|')
-                dict_ingredients = {'ingredient_name': ingredient[0].strip(' '), 'quantity': ingredient[1].strip(' '), 'measure': ingredient[2].strip(' \n')}
+                dict_ingredients = {'ingredient_name': ingredient[0].strip(' '), 'quantity': int(ingredient[1].strip(' ')), 'measure': ingredient[2].strip(' \n')}
                 list.append(dict_ingredients)
                 a -= 1
             cook_book[dish] = list
@@ -16,6 +16,16 @@ def create_cook_book(document):
                 break
         return cook_book
 
-print(create_cook_book('recipes.txt'))
+#print(cook_book())
 
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book()[dish]:
+            if ingredient['ingredient_name'] not in shop_list:
+                shop_list[ingredient['ingredient_name']] = {'measure':ingredient['measure'], 'quantity':ingredient['quantity'] * person_count}
+            else:
+                shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
+    return shop_list
 
+print(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 1))
